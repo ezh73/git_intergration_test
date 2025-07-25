ead2e182d6e24f339c45310740b36203c253206e
ezh73 <ezh737@gmail.com>
2025-07-25 10:38:08 +0900

    충돌 유발

---
+++ calculator.java
@@ -0,0 +1,11 @@
+// 작업 2
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("1+1=2");
+        System.out.println("1+1=0");
+        System.out.println("2*4=8");
+        System.out.println("4/2=2");
+        System.out.println("4*9=36");
+    }
+}
+
